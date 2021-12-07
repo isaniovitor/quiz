a2c6c9740ddbeec28caee8609e2c410f62d96263
Isânio Vitor Félix Araujo <isanio.araujo@lead.fortaleza>
2021-12-07 08:49:48 -0300

    project config

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,17 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    presets: ['module:metro-react-native-babel-preset', 'babel-preset-expo'],
+
+    plugins: [
+      [
+        'babel-plugin-root-import',
+        {
+          rootPathPrefix: '~',
+          rootPathSuffix: 'src',
+        },
+      ],
+    ],
   };
 };
+
